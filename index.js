8120d1573aed972b37fe9818d1354da0c3a37436
Asad <aliasad71734@gmail.com>
2020-06-07 15:43:19 +0500

    Add gitignore nad update ports

--- index.js
+++ index.js
@@ -48,6 +48,12 @@ index.get("/bp", function(req,res){
     });
 });
 
-index.listen(3000,function(){
-    console.log("Server started on port 3000");
+let port = process.env.PORT;
+
+if(port == null || port == ""){
+    port = 3000;
+}
+
+index.listen(port,function(){
+    console.log("Server has started");
 });
